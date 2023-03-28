3be61aa4d8644ea8a4940d5535aaf7a731221b27
AntonJames-Sistence <anton.james.ja@gmail.com>
2023-03-28 17:27:54 -0400

    add initialize for piece' git push git status '

--- Chess/piece.rb
+++ Chess/piece.rb
@@ -1,5 +1,16 @@
 class Piece
-    def initialize
-        #empty piece for now, to use it temporary for the board
+    def initialize(color, board, pos)
+        @color = color
+        @board = board
+        @pos = pos
+    end
+
+    def valid_moves
+    end
+
+    def pos=(val)
+    end
+
+    def move_into_check?(end_pos)
     end
 end
