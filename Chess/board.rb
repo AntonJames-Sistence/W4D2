4de7e64f315c8a69c368b8d2aca0849dfd28c15d
AntonJames-Sistence <anton.james.ja@gmail.com>
2023-03-28 15:43:21 -0400

    Add Board and initialize method

---
+++ Chess/board.rb
@@ -0,0 +1,20 @@
+class Piece
+    def initialize
+    end
+end
+
+
+class Board
+    def initialize
+        @board = Array.new(8) { Array.new(8) {nil} }
+    end
+
+    def [](pos)
+    end
+
+    def []=(pos)
+    end
+
+    def move_piece(start_pos, end_pos)
+    end
+end
