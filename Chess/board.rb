809cacd0620a639dca99a8d49218b0e326dd2c48
AntonJames-Sistence <anton.james.ja@gmail.com>
2023-03-28 16:50:48 -0400

    Add raising errors for get position method, Add two errors for move_piece method. Test for phase 1

--- Chess/board.rb
+++ Chess/board.rb
@@ -26,6 +26,9 @@ class Board
 
     def [](pos)
         row, col = pos
+        if row > 7 || row < 0 || col > 7 || col < 0
+            raise "Position is not valid!"
+        end
         @board[row][col]
     end
 
@@ -35,13 +38,31 @@ class Board
     end
 
     def move_piece(start_pos, end_pos)
+        row, col = end_pos
+        if row > 7 || row < 0 || col > 7 || col < 0
+            raise "Can't move to the chosen position"
+        end
+
         piece = self.[](start_pos)
+        raise "There is no piece at this position!" if piece == nil
+
         self.[]=(end_pos, piece)
         self.[]=(start_pos, nil)
+        puts "Move is successful"
     end
 end
 
 board = Board.new
 p board
-board.move_piece([6, 1], [4, 4])
+
+board.move_piece([6, 2], [4, 4]) # move piece test #1 passed
 p board
+
+# board.move_piece([-4, -6], [4, 4]) # move piece test #2 partle passed
+# p board
+
+# board.move_piece([6, 2], [-2, -2]) # move piece test #3 passed
+# p board
+
+# board.move_piece([-1, -1], [4, 4]) # move piece test #4
+# p board
