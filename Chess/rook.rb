20fd50c0a3eee3bfa801805ae8d1452825bfa13f
AntonJames-Sistence <anton.james.ja@gmail.com>
2023-03-29 16:46:37 -0400

    add move_dirs for rook class

--- Chess/rook.rb
+++ Chess/rook.rb
@@ -2,7 +2,7 @@ require_relative 'piece.rb'
 require_relative 'slideable.rb'
 
 class Rook < Piece
-    require Slideable
+    include Slideable
 
     def initialize(color, board, pos)
         super(color, board, pos)
@@ -12,10 +12,22 @@ class Rook < Piece
         '♜'.colorize(color)
     end
     
-    protected
+    # protected
     
-    def move_dirs
-        
+    def move_dirs # support link for directions
+        row, col = self.pos
+        rook_possible_moves = []
+
+        HORIZONTAL_DIRS.each do |hor_ver_directions|
+            row_dir, col_dir = hor_ver_directions
+            rook_possible_moves << [row+row_dir, col+col_dir]
+        end
+
+        rook_possible_moves
     end
 
 end
+
+rook = Rook.new(nil, nil, [5, 3])
+p rook
+p rook.move_dirs
