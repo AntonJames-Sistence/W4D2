aef822cc6e676c2f325443cc9ebcaee449fdf6e0
AntonJames-Sistence <anton.james.ja@gmail.com>
2023-03-29 14:34:03 -0400

    Make moves for Stepable

--- Chess/stepable.rb
+++ Chess/stepable.rb
@@ -1,22 +1,21 @@
 module Stepable
-    # def moves
-      # create array to collect moves
-    #     possible_moves = []
+    def moves
+        # create array to collect moves
+        possible_moves = []
   
-    #   # iterate through each of the piece's possible move_diffs
-    #     self.move_diffs.each do |possible_position| #using on inst of a piece
-    #         # check if this pos is on a board ???
-    #         # is occupied with piece of same colour?
-    #         self.board.[](possible_position)
+        # iterate through each of the piece's possible move_diffs
+        self.move_diffs.each do |possible_piece_position| #using on inst of a piece
+            row, col = possible_piece_position # deconstract pos option
 
-    #     # for each move_diff, increment the piece's position to generate a new position
-    #     # add the new position to the moves array if it is:
-    #       # on the board and empty
-    #       # OR on the board and contains a piece of the opposite color
-  
-    #   # return the final array of moves
-    #     possible_moves
-    # end
+            attempted_pos = self.board[row][col]  #access to boards pos
+            
+            if attempted_pos.is_a?(NullPiece) || attempted_pos.color != self.color # check for empty spot or enemy piece, also eliminate problem with outside board pos or ally piece
+                possible_moves << possible_piece_position
+            end
+        end
+        
+        possible_moves # return the final array of moves
+    end
   
     private
   
